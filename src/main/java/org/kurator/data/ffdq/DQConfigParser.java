5f66ef0826313561e870fa9e16ed668bbc706d03
lowery <lowery@cs.umb.edu>
2016-11-07 14:38:03 -0500

    Implementing dq report generation feature and string substitution
    Added string substitution of values defined in GlobalContext for mechanism. Created a generateReport method for producing a DQReport object from an instance of BaseRecord.

--- src/main/java/org/kurator/data/ffdq/DQConfigParser.java
+++ src/main/java/org/kurator/data/ffdq/DQConfigParser.java
@@ -1,9 +1,17 @@
 package org.kurator.data.ffdq;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.text.StrSubstitutor;
+import org.kurator.data.ffdq.assertions.*;
+import org.kurator.data.provenance.BaseRecord;
+import org.kurator.data.provenance.CurationStep;
+import org.kurator.data.provenance.GlobalContext;
+import org.kurator.data.provenance.NamedContext;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by lowery on 11/2/16.
@@ -30,6 +38,76 @@ public class DQConfigParser {
 
     public AssertionsConfig getAssertions() {
         return assertions;
+    }
+
+    public DQReport generateReport(BaseRecord result) {
+        DQReport report = new DQReport();
+
+        GlobalContext globalContext = result.getGlobalContext();
+        StrSubstitutor contextProps = new StrSubstitutor(globalContext.getProperties());
+
+        Map<NamedContext, List<CurationStep>> curationStepMap =
+                result.getCurationHistoryContexts();
+
+        for (NamedContext context : curationStepMap.keySet()) {
+            Assertion assertion = assertions.forContext(context.getName());
+
+            if (assertion instanceof Measure) {
+                Measure measure = (Measure) assertion;
+
+                String parsedMechanism = contextProps.replace(measure.getMechanism());
+                measure.setMechanism(parsedMechanism);
+
+                System.out.println("MEASURE: ");
+                System.out.println("    dimension : " + measure.getDimension() + "\n    specification : "
+                        + measure.getSpecification() + "\n    mechanism : " + measure.getMechanism());
+
+                report.pushMeasure(measure);
+            } else if (assertion instanceof Validation) {
+                Validation validation = (Validation) assertion;
+
+                String parsedMechanism = contextProps.replace(validation.getMechanism());
+                validation.setMechanism(parsedMechanism);
+
+                System.out.println("VALIDATION: ");
+                System.out.println("    criterion : " + validation.getCriterion() + "\n    specification : "
+                        + validation.getSpecification() + "\n    mechanism : " + validation.getMechanism());
+
+                report.pushValidation(validation);
+            } else if (assertion instanceof Improvement) {
+                Improvement improvement = (Improvement) assertion;
+
+                String parsedMechanism = contextProps.replace(improvement.getMechanism());
+                improvement.setMechanism(parsedMechanism);
+
+                System.out.println("IMPROVEMENT: ");
+                System.out.println("    enhancement : " + improvement.getEnhancement() + "\n    specification : "
+                        + improvement.getSpecification() + "\n    mechanism : " + improvement.getMechanism());
+
+                report.pushImprovement(improvement);
+            }
+            System.out.println();
+
+            System.out.println("    context : " + context.getName());
+            if (!context.getFieldsActedUpon().isEmpty()) {
+                System.out.println("    fieldsActedUpon : " + context.getFieldsActedUpon());
+            }
+            if (!context.getFieldsConsulted().isEmpty()) {
+                System.out.println("    fieldsConsulted : " + context.getFieldsConsulted());
+            }
+
+            System.out.println();
+
+            List<CurationStep> steps = curationStepMap.get(context);
+
+            for (CurationStep step : steps) {
+                System.out.print("    state: " + step.getCurationStatus() + "\n    comments: " + step.getCurationComments());
+                System.out.println();
+                System.out.println();
+            }
+        }
+
+        return report;
     }
 
     public static void main(String[] args) throws IOException {
