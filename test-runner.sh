726334b97692d19f24394a85dfddbf969287c8b6
chicoreus <mole@morris.net>
2022-01-07 18:06:42 -0500

    Version consistency.  Updating shell scripts to point to current version of jar artifact in target/ 2.0.1-SNAPSHOT.

--- test-runner.sh
+++ test-runner.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-java -cp "$parent_path"/target/kurator-ffdq-2.0.0-SNAPSHOT.jar:./* org.datakurator.ffdq.runner.TestRunner "$@"
+java -cp "$parent_path"/target/kurator-ffdq-2.0.1-SNAPSHOT.jar:./* org.datakurator.ffdq.runner.TestRunner "$@"
