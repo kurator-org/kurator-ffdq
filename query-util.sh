8886455165468d8ac001071d6eac765039f27288
chicoreus <mole@morris.net>
2023-09-20 15:38:49 -0400

    Release version 2.0.2 for archival purposes.  Updating references in shell scripts.

--- query-util.sh
+++ query-util.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-java -cp "$parent_path"/target/kurator-ffdq-2.0.2-SNAPSHOT.jar org.datakurator.ffdq.util.QueryUtil "$@"
+java -cp "$parent_path"/target/kurator-ffdq-2.0.2.jar org.datakurator.ffdq.util.QueryUtil "$@"
