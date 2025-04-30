203790b11daf92b748c6f59d15d806e4ee4b90bb
chicoreus <mole@morris.net>
2025-04-30 09:14:16 -0400

    Updating version references in the shell scripts.

--- test-runner.sh
+++ test-runner.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-java -cp "$parent_path"/target/kurator-ffdq-3.0.0.jar:./* org.datakurator.ffdq.runner.TestRunner "$@"
+java -cp "$parent_path"/target/kurator-ffdq-3.2.0-SNAPSHOT.jar:./* org.datakurator.ffdq.runner.TestRunner "$@"
